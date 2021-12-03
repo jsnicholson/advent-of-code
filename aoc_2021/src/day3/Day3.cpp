d442be3ecfa383ec1ce70cb35f5c4da0695b6ce1
Josh Nicholson <jsnicholson08@gmail.com>
2021-12-03 18:58:53 +0000

    Day3 Part1

--- aoc_2021/src/day3/Day3.cpp
+++ aoc_2021/src/day3/Day3.cpp
@@ -5,10 +5,27 @@
 #include <iostream>
 
 void Day3::Parse() {
+    for (std::list<std::string>::iterator it = data.begin(); it != data.end(); it++) {
+        listBitset.push_back(std::bitset<bitsetLength>(*it));
+    }
 }
 
 int Day3::Part1() {
-    return -1;
+    int total = 0;
+    std::bitset<bitsetLength> gamma{ "" }, epsilon{ "" };
+
+    for (int i = 0; i < bitsetLength; i++) {
+        total = 0;
+
+        for (const auto& bitset : listBitset) {
+            total += (bitset[i]) ? 1 : -1;
+        }
+
+        gamma.set(i, total >= 0);
+        epsilon.set(i, total < 0);
+    }
+
+    return static_cast<int>(gamma.to_ulong()) * static_cast<int>(epsilon.to_ulong());
 }
 
 int Day3::Part2() {
--- aoc_2021/src/day3/Day3.h
+++ aoc_2021/src/day3/Day3.h
@@ -2,11 +2,15 @@
 #include "Day.h"
 #include <string>
 #include <list>
+#include <bitset>
 
 class Day3 : public Day {
-
 private:
 	void Parse() override;
 	int Part1() override;
 	int Part2() override;
+
+private:
+	static const size_t bitsetLength = 12;
+	std::list<std::bitset<bitsetLength>> listBitset;
 };
