206fe9d56173302eefe8dcb496845d914515e843
Josh Nicholson <jsnicholson08@gmail.com>
2021-12-03 11:59:17 +0000

    Cleaned up main file by moving functions to Util

--- aoc_2021/src/main.cpp
+++ aoc_2021/src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-#include "Days.hpp"
 #include "Utils.hpp"
 
 const char* banner = R"(
@@ -21,77 +20,6 @@ const char* banner = R"(
         /___\ |[] _ []|    //|\\    | A /^\ A |    |[] _ []| _.O,_
     ....|"#"|.|  |*|  |...///|\\\...|   |"|   |....|  |*|  |..(^).... )";
 
-template <typename T>
-void RunDay() {
-    std::string typeName = std::string(typeid(T).name());
-    std::string dayName = typeName.substr(typeName.find(" ") + 1);
-
-    std::cout << "Opening " << dayName << "..." << std::endl;
-
-    Day* day = new T();
-
-    std::string inputfilename = std::string("resources\\input_") + Utils::ToLower(dayName) + std::string(".txt");
-
-    day->Run(inputfilename);
-}
-
-void OpenDay(int day) {
-    switch (day) {
-    case 1:
-        RunDay<Day1>();break;
-    case 2:
-        RunDay<Day2>();break;
-    case 3:
-        RunDay<Day3>();break;
-    case 4:
-        RunDay<Day4>();break;
-    case 5:
-        RunDay<Day5>();break;
-    case 6:
-        RunDay<Day6>(); break;
-    case 7:
-        RunDay<Day7>(); break;
-    case 8:
-        RunDay<Day8>(); break;
-    case 9:
-        RunDay<Day9>(); break;
-    case 10:
-        RunDay<Day10>(); break;
-    case 11:
-        RunDay<Day11>(); break;
-    case 12:
-        RunDay<Day12>(); break;
-    case 13:
-        RunDay<Day13>(); break;
-    case 14:
-        RunDay<Day14>(); break;
-    case 15:
-        RunDay<Day15>(); break;
-    case 16:
-        RunDay<Day16>(); break;
-    case 17:
-        RunDay<Day17>(); break;
-    case 18:
-        RunDay<Day18>(); break;
-    case 19:
-        RunDay<Day19>(); break;
-    case 20:
-        RunDay<Day20>(); break;
-    case 21:
-        RunDay<Day21>(); break;
-    case 22:
-        RunDay<Day22>(); break;
-    case 23:
-        RunDay<Day23>(); break;
-    case 24:
-        RunDay<Day24>(); break;
-    case 25:
-        RunDay<Day25>(); break;
-    default:
-        std::cout << "That day isnt in the advent!" << std::endl;
-    }
-}
-
 int main(int argc, char* argv[]) {
     std::cout << banner << "\n\n" << std::endl;
 
@@ -99,7 +27,7 @@ int main(int argc, char* argv[]) {
     std::cout << "Which day are you opening?" << std::endl;
     std::cin >> day;
 
-    OpenDay(day);
+    Utils::OpenDay(day);
 
 	return 0;
 }
