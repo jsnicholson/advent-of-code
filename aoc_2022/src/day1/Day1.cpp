11899b0238bd4ecbfaa249d15bb137d4c6bd50b1
Josh Nicholson <jsnicholson08@gmail.com>
2022-12-02 10:32:13 +0000

    update day1 so the responsibility is better split
    parse only gets the data into a useful format, the summing should be done separately

--- aoc_2022/src/day1/Day1.cpp
+++ aoc_2022/src/day1/Day1.cpp
@@ -1,31 +1,48 @@
 #include "day1.h"
 
 #include <algorithm>
+#include <numeric>
 
 void Day1::Parse() {
-    int calorie = 0;
+    // parse input to a vector<int> for each elf (each set of calories)
+    std::vector<int> calories = {};
     for (std::list<std::string>::iterator it = data.begin(); it != data.end(); it++) {
         if (it->empty()) {
-            calories.push_back(calorie);
-            calorie = 0;
+            m_vecCaloriesPerElf.push_back(calories);
+            calories.clear();
+            continue;
         }
-        else {
-            calorie += std::stoi(*it);
-        }
+        
+        calories.push_back(std::stoi(*it));
     }
-    if (calorie > 0)
-        calories.push_back(calorie);
+    if (!calories.empty())
+        m_vecCaloriesPerElf.push_back(calories);
 }
 
 int Day1::Part1() {
+    auto calories = SumEachElfsCalories();
     return *std::max_element(calories.begin(), calories.end());
 }
 
 int Day1::Part2() {
-    calories.sort();
+    auto calories = SumEachElfsCalories();
+    std::sort(calories.begin(), calories.end());
+
+    // count last three values
     auto iterator = calories.end();
     int count = 0;
     for (int i = 0; i < 3; i++)
         count += *--iterator;
+
     return count;
+}
+
+std::vector<int> Day1::SumEachElfsCalories() { 
+    std::vector<int> sums = {};
+    for (std::vector<std::vector<int>>::iterator it = m_vecCaloriesPerElf.begin(); it != m_vecCaloriesPerElf.end(); it++) {
+        auto elfCalories = *it;
+        auto result = std::accumulate(elfCalories.begin(), elfCalories.end(), 0);
+        sums.push_back(result);
+    }
+    return sums;
 }
--- aoc_2022/src/day1/Day1.h
+++ aoc_2022/src/day1/Day1.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "Day.h"
 
-#include <list>
+#include <vector>
 
 class Day1 : public Day {
 public:
-	std::list<int> calories;
+	std::vector<std::vector<int>> m_vecCaloriesPerElf;
 
 public:
 	void Parse() override;
 	int Part1() override;
 	int Part2() override;
+
+public:
+	std::vector<int> SumEachElfsCalories();
 };
