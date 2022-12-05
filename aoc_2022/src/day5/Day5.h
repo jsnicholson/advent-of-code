56231f9809a1bb9486acf53940eeb516d18ff833
Josh Nicholson <jsnicholson08@gmail.com>
2022-12-05 14:23:20 +0000

    complete day 5

--- aoc_2022/src/day5/Day5.h
+++ aoc_2022/src/day5/Day5.h
@@ -1,9 +1,26 @@
 #pragma once
 #include "Day.h"
 
+#include <stack>
+#include <vector>
+
 class Day5 : public Day {
+public:
+	typedef std::stack<char> cratestack;
+	// how many to move by, from stack to stack
+	typedef std::pair<int, std::pair<int,int>> instruction;
+
+	std::vector<cratestack> m_cratestacks;
+	std::vector<instruction> m_instructions;
+
 private:
 	void Parse() override;
 	int Part1() override;
 	int Part2() override;
+
+public:
+	std::vector<std::string> Split(std::string string, std::string delimiter);
+	std::vector<std::string> Split(std::string string, char delimiter);
+
+	instruction ParseInstruction(std::string);
 };
