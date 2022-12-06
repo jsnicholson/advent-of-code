7c16b415bb01705a4ce81a712ae76f4acebc9456
Joshua Nicholson <jonic@netcompany.com>
2022-12-06 10:20:37 +0000

    complete day 6

--- aoc_2022/src/day6/Day6.cpp
+++ aoc_2022/src/day6/Day6.cpp
@@ -1,14 +1,54 @@
 #include "Day6.h"
 
-#include <iostream>
+#include <queue>
+#include <set>
+#include <cmath>
 
 void Day6::Parse() {
+    m_signal = data.front();
 }
 
 std::string Day6::Part1() {
-    return std::string("unimplemented");
+    int indexOfPacketMarker = 0;
+    std::string buffer;
+    for (std::string::iterator it = m_signal.begin(); it != m_signal.end(); it++) {
+        buffer.push_back(*it);
+        if (buffer.length() > 4) {
+            buffer.erase(0, 1);
+        }
+        if (buffer.length() == 4) {
+            if (StringHasNoDuplicates(buffer)) {
+                indexOfPacketMarker = abs(std::distance(it, m_signal.begin())) + 1;
+                break;
+            }
+        }
+    }
+    return std::to_string(indexOfPacketMarker);
 }
 
 std::string Day6::Part2() {
-    return std::string("unimplemented");
+    int markerLength = 14;
+    int indexOfPacketMarker = 0;
+    std::string buffer;
+    for (std::string::iterator it = m_signal.begin(); it != m_signal.end(); it++) {
+        buffer.push_back(*it);
+        if (buffer.length() > markerLength) {
+            buffer.erase(0, 1);
+        }
+        if (buffer.length() == markerLength) {
+            if (StringHasNoDuplicates(buffer)) {
+                indexOfPacketMarker = abs(std::distance(it, m_signal.begin())) + 1;
+                break;
+            }
+        }
+    }
+    return std::to_string(indexOfPacketMarker);
+}
+
+bool Day6::StringHasNoDuplicates(std::string string) {
+    std::set<char> check_uniq;
+    for (unsigned long int i = 0; i < string.length(); ++i)
+        if (!check_uniq.insert(string[i]).second)
+            return false; // Duplicated char found
+    return true; // No duplicated char found
 }
--- aoc_2022/src/day6/Day6.h
+++ aoc_2022/src/day6/Day6.h
@@ -3,9 +3,14 @@
 #include <string>
 
 class Day6 : public Day {
+public:
+	std::string m_signal;
 
 private:
 	void Parse() override;
 	std::string Part1() override;
 	std::string Part2() override;
+
+public:
+	bool StringHasNoDuplicates(std::string);
 };
