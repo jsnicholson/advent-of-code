60ccf25d1d1f7b044b8b4807f02d42819211388f
Josh Nicholson <jsnicholson08@gmail.com>
2022-12-11 00:30:00 +0000

    clean up code a lot
    move to proper functions, still a little to do

--- aoc_2022/src/day10/Day10.cpp
+++ aoc_2022/src/day10/Day10.cpp
@@ -13,14 +13,13 @@ std::string Day10::Part1() {
     for (std::vector<std::string>::const_iterator it = m_instructions.begin(); it != m_instructions.end(); it++) {
         std::string instruction = *it;
         auto tokens = Utils::Split(instruction, ' ');
-        std::map<int, int> signalStrengths{};
-        if (tokens[0] == "noop") {
-            signalStrengths = ProcessNoopCommand(cycle, xRegister);
-        } else if (tokens[0] == "addx") {
-            signalStrengths= ProcessAddXCommand(std::stoi(tokens[1]), cycle, xRegister);
+        cycle++;
+        UpdateInterestingValues(cycle, xRegister, interestingSignalStrengths);      
+        if (tokens[0] == "addx") {
+            xRegister += std::stoi(tokens[1]);
+            cycle++;
+            UpdateInterestingValues(cycle, xRegister, interestingSignalStrengths);     
         }
-
-        UpdateInterestingValues(signalStrengths, interestingSignalStrengths);
     }
 
     size_t sum = std::accumulate(interestingSignalStrengths.begin(), interestingSignalStrengths.end(), 0,
@@ -30,55 +29,32 @@ std::string Day10::Part1() {
     return std::to_string(sum);
 }
 
+// part2 doesnt produce a direct value, but produces a output that resembles drawn characters
 std::string Day10::Part2() {
     std::string output{"\n "};
-    int xRegister = 1, cycle = 1;
+    int xRegister = 1, cycle = 0;
     
     for (std::vector<std::string>::const_iterator it = m_instructions.begin(); it != m_instructions.end(); it++) {
         std::string instruction = *it;
         auto tokens = Utils::Split(instruction, ' ');
-        if (cycle % m_screenWidth == 0)
-            output.append("\n");     
-        (IsPixelLit(xRegister, cycle)) ? output.append("#") : output.append(" ");
+        
         cycle++;
+        ProduceScreenOutput(cycle, xRegister, output);
         
         if (tokens[0] == "addx") {
             xRegister += std::stoi(tokens[1]);
-            if (cycle % m_screenWidth == 0)
-                output.append("\n");
-            (IsPixelLit(xRegister, cycle)) ? output.append("#") : output.append(" ");
+            
             cycle++;
+            ProduceScreenOutput(cycle, xRegister, output);
         }
     }
     return output;
 }
 
-std::map<int,int> Day10::ProcessNoopCommand(int& cycle, const int xRegister) {
-    cycle++;
-    return std::map<int, int>{ {cycle,cycle*xRegister} };
-}
-
-std::map<int,int> Day10::ProcessAddXCommand(const int addValue, int& cycle, int& xRegister) {
-    std::map<int, int> signalStrengths{};
-    cycle++;
-    signalStrengths.insert({cycle, cycle*xRegister});
-
-    cycle++;
-    xRegister += addValue;
-    signalStrengths.insert({ cycle, cycle * xRegister });
-    
-    return signalStrengths;
-}
-
-void Day10::UpdateInterestingValues(const std::map<int, int> signalStrengths, std::map<int, int>& interestingSignalStrengths) {
-    std::map<int, int>::iterator it;
-    for (const auto& signalStrength : signalStrengths) {
-        const int cycle = signalStrength.first;
-        it = interestingSignalStrengths.find(cycle);
-        if (it != interestingSignalStrengths.end()) {
-            it->second = signalStrength.second;
-        }
-    }
+void Day10::UpdateInterestingValues(const int cycle, const int xRegister, std::map<int, int>& interestingSignalStrengths) {
+    std::map<int, int>::iterator it = interestingSignalStrengths.find(cycle);
+    if (it != interestingSignalStrengths.end())
+        it->second = cycle * xRegister;
 }
 
 bool Day10::IsPixelLit(const int xRegister, const int cathodeRayIndex) {
--- aoc_2022/src/day10/Day10.h
+++ aoc_2022/src/day10/Day10.h
@@ -15,9 +15,7 @@ private:
 	std::string Part1() override;
 	std::string Part2() override;
 
-	std::map<int,int> ProcessNoopCommand(int& cycle, const int xRegister);
-	std::map<int,int> ProcessAddXCommand(const int addValue, int& cycle, int& xRegister);
-	void UpdateInterestingValues(const std::map<int,int> signalStrengths, std::map<int,int>& interestingSignalStrengths);
+	void UpdateInterestingValues(const int cycle, const int xRegister, std::map<int, int>& interestingSignalStrengths);
 	bool IsPixelLit(const int xRegister, const int cathodeRayIndex);
 	void ProduceScreenOutput(const int cycle, const int xRegister, std::string& output);
 };
