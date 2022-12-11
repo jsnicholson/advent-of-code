6b5adeb33a8290dc7c2817bf205d012dc6ac3b10
Josh Nicholson <jsnicholson08@gmail.com>
2022-12-11 16:59:27 +0000

    started on day 11

--- aoc_2022/src/day11/Day11.cpp
+++ aoc_2022/src/day11/Day11.cpp
@@ -1,6 +1,25 @@
 #include "Day11.h"
 
+#include "Utils.hpp"
+
 void Day11::Parse() {
+    std::string monkeyPlainTextDescription{};
+    for (std::vector<std::string>::const_iterator it = data.begin(); it != data.end(); it++) {
+        std::string line = *it;
+        if (line.empty()) {
+            m_monkeys.push_back(new Monkey(monkeyPlainTextDescription));
+            monkeyPlainTextDescription.clear();
+            continue;
+        }
+
+        if (DoesStringStartWithToken(line, "Monkey")) {
+            continue;
+        }
+
+        monkeyPlainTextDescription.append(((!monkeyPlainTextDescription.empty()) ? std::string("\n") : std::string("")) + line);
+    }
+    if (!monkeyPlainTextDescription.empty())
+        m_monkeys.push_back(new Monkey(monkeyPlainTextDescription));
 }
 
 std::string Day11::Part1() {
@@ -9,4 +28,72 @@ std::string Day11::Part1() {
 
 std::string Day11::Part2() {
     return std::string("unimplemented");
+}
+
+bool Day11::DoesStringStartWithToken(const std::string string, const std::string token) {
+    return string.rfind(token, 0) == 0;
+}
+
+Monkey::Monkey(std::string plainTextDescription) : m_testDivisibleBy(0), m_ifTestTrueThrowToMonkeyNumber(0), m_ifTestFalseThrowToMonkeyNumber(0) {
+    std::vector<std::string> lines = Utils::Split(plainTextDescription, '\n');
+    size_t found;
+    for (std::vector<std::string>::const_iterator it = lines.begin(); it != lines.end(); it++) {
+        std::string line = *it;
+        for (const auto& entry : Monkey::m_mapTokensToProcess) {
+            if (found = line.find(entry.first) != std::string::npos)
+                entry.second(line.substr(found + entry.first.length() + 1));
+        }
+
+        //if (found = line.find(m_tokenStartingItems) != std::string::npos)
+        //    ProcessStartingItems(line.substr(found + m_tokenStartingItems.length() + 1));
+        //else if (found = line.find(m_tokenOperation) != std::string::npos)
+        //    ProcessOperation(line.substr(found + m_tokenOperation.length() + 1));
+        //else if (found = line.find(m_tokenTestDivisbleBy) != std::string::npos)
+        //    ProcessTest(line.substr(found + m_tokenTestDivisbleBy.length() + 1));
+        //else if (found = line.find(m_tokenTrue) != std::string::npos)
+        //    ProcessTestTrue(line.substr(found + m_tokenTrue.length() + 1));
+        //else if (found = line.find(m_tokenFalse) != std::string::npos)
+        //    ProcessTestFalse(line.substr(found + m_tokenFalse.length() + 1));
+    }
+}
+
+void Monkey::ProcessStartingItems(const std::string startingItemsString) {
+    const auto values = Utils::Split(startingItemsString, ", ");
+    for (int count = 0; count < values.size(); count++)
+        m_items.push_back(std::stoi(values[count]));
+}
+
+void Monkey::ProcessOperation(const std::string operationString) {
+    const auto tokens = Utils::Split(operationString, ' ');
+    const char op = tokens[3][0];
+    const TermType termType = (tokens[4] != "old") ? TermType::constant : TermType::variable;
+    const int term = (termType == TermType::constant) ? std::stoi(tokens[4]) : INT_MIN;
+    m_operation = [op, term, termType](int& valueToUpdate) {
+        if (termType == TermType::constant) {
+            if (op == '+')
+                valueToUpdate += term;
+            else if (op == '*')
+                valueToUpdate *= term;
+        } else if (termType == TermType::variable) {
+            if (op == '+')
+                valueToUpdate += valueToUpdate;
+            else if (op == '*')
+                valueToUpdate *= valueToUpdate;
+        }
+    };
+}
+
+void Monkey::ProcessTest(const std::string testString) {
+    const auto tokens = Utils::Split(testString, ' ');
+    m_testDivisibleBy = std::stoi(tokens[2]);
+}
+
+void Monkey::ProcessTestTrue(const std::string testTrueString) {
+    const auto tokens = Utils::Split(testTrueString, ' ');
+    m_ifTestTrueThrowToMonkeyNumber = std::stoi(tokens[4]);
+}
+
+void Monkey::ProcessTestFalse(const std::string testFalseString) {
+    const auto tokens = Utils::Split(testFalseString, ' ');
+    m_ifTestFalseThrowToMonkeyNumber = std::stoi(tokens[4]);
 }
--- aoc_2022/src/day11/Day11.h
+++ aoc_2022/src/day11/Day11.h
@@ -1,12 +1,55 @@
 #pragma once
 #include "Day.h"
 
-#include <string>
+#include <functional>
+#include <map>
+
+class Monkey {
+public:
+	// plainTextDescription needs to be parsed to fill out members
+	Monkey(std::string plainTextDescription);
+
+private:
+	std::vector<uint64_t> m_items;
+	std::function<void(int&)> m_operation;
+	int m_testDivisibleBy, m_ifTestTrueThrowToMonkeyNumber, m_ifTestFalseThrowToMonkeyNumber;
+
+private:
+	const std::string m_tokenStartingItems = "Starting items: ";
+	const std::string m_tokenOperation = "Operation: ";
+	const std::string m_tokenTestDivisbleBy = "Test: ";
+	const std::string m_tokenTrue = "If true: ";
+	const std::string m_tokenFalse = "If false: ";
+
+	const enum TermType {
+		constant = 1,
+		variable = 2
+	};
+
+	const std::map<std::string, std::function<void(std::string)>> m_mapTokensToProcess = {
+		{ m_tokenStartingItems, [=](std::string string) {this->ProcessStartingItems(string); } },
+		{ m_tokenOperation, [=](std::string string) {this->ProcessOperation(string); } },
+		{ m_tokenTestDivisbleBy, [=](std::string string) {this->ProcessTest(string); } },
+		{ m_tokenTrue, [=](std::string string) {this->ProcessTestTrue(string); } },
+		{ m_tokenFalse, [=](std::string string) {this->ProcessTestFalse(string); } }
+	};
+
+private:
+	void ProcessStartingItems(const std::string);
+	void ProcessOperation(const std::string);
+	void ProcessTest(const std::string);
+	void ProcessTestTrue(const std::string);
+	void ProcessTestFalse(const std::string);
+};
 
 class Day11 : public Day {
+public:
+	std::vector<Monkey*> m_monkeys;
 
 private:
 	void Parse() override;
 	std::string Part1() override;
 	std::string Part2() override;
+
+	bool DoesStringStartWithToken(const std::string string, const std::string token);
 };
