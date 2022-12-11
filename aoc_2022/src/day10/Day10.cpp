0b199125a58dad534e849d49ac06de89e1a33665
Josh Nicholson <jsnicholson08@gmail.com>
2022-12-11 00:39:23 +0000

    final cleanup
    makes instruction execution same across both instances, just returning a list of states that the important vars were in. then afterwards we can calculate out the values that we want

--- aoc_2022/src/day10/Day10.cpp
+++ aoc_2022/src/day10/Day10.cpp
@@ -12,14 +12,8 @@ std::string Day10::Part1() {
     
     for (std::vector<std::string>::const_iterator it = m_instructions.begin(); it != m_instructions.end(); it++) {
         std::string instruction = *it;
-        auto tokens = Utils::Split(instruction, ' ');
-        cycle++;
-        UpdateInterestingValues(cycle, xRegister, interestingSignalStrengths);      
-        if (tokens[0] == "addx") {
-            xRegister += std::stoi(tokens[1]);
-            cycle++;
-            UpdateInterestingValues(cycle, xRegister, interestingSignalStrengths);     
-        }
+        auto states = ExecuteInstruction(cycle, xRegister, instruction);
+        StatesToInterestingSignalStrengths(states, interestingSignalStrengths);
     }
 
     size_t sum = std::accumulate(interestingSignalStrengths.begin(), interestingSignalStrengths.end(), 0,
@@ -36,34 +30,49 @@ std::string Day10::Part2() {
     
     for (std::vector<std::string>::const_iterator it = m_instructions.begin(); it != m_instructions.end(); it++) {
         std::string instruction = *it;
-        auto tokens = Utils::Split(instruction, ' ');
-        
-        cycle++;
-        ProduceScreenOutput(cycle, xRegister, output);
-        
-        if (tokens[0] == "addx") {
-            xRegister += std::stoi(tokens[1]);
-            
-            cycle++;
-            ProduceScreenOutput(cycle, xRegister, output);
-        }
+        auto states = ExecuteInstruction(cycle, xRegister, instruction);
+        StatesToScreenOutput(states, output);
     }
     return output;
 }
 
-void Day10::UpdateInterestingValues(const int cycle, const int xRegister, std::map<int, int>& interestingSignalStrengths) {
-    std::map<int, int>::iterator it = interestingSignalStrengths.find(cycle);
-    if (it != interestingSignalStrengths.end())
-        it->second = cycle * xRegister;
+std::vector<Day10::state> Day10::ExecuteInstruction(int& cycle, int& xRegister, const std::string instruction) {
+    std::vector<state> states{};
+    cycle++;
+    states.push_back(std::make_pair(cycle, xRegister));
+
+    auto tokens = Utils::Split(instruction, ' ');
+    if (tokens[0] == "addx") {
+        xRegister += std::stoi(tokens[1]);
+        cycle++;
+        states.push_back(std::make_pair(cycle, xRegister));
+    }
+
+    return states;
+}
+
+void Day10::StatesToInterestingSignalStrengths(const std::vector<state> states, std::map<int, int>& interestingSignalStrengths) {
+    std::map<int, int>::iterator it;
+    for (const auto state : states) {
+        const int cycle = state.first;
+        const int xRegister = state.second;
+        it = interestingSignalStrengths.find(cycle);
+        if (it != interestingSignalStrengths.end())
+            it->second = cycle * xRegister;
+    }
+}
+
+void Day10::StatesToScreenOutput(const std::vector<state> states, std::string& output) {
+    for (const auto state : states) {
+        const int cycle = state.first;
+        const int xRegister = state.second;
+        if (cycle % m_screenWidth == 0)
+            output.append("\n");
+        IsPixelLit(xRegister, cycle) ? output.append("#") : output.append(" ");
+    }
 }
 
 bool Day10::IsPixelLit(const int xRegister, const int cathodeRayIndex) {
     const int horizontalLocation = cathodeRayIndex % m_screenWidth;
     return (xRegister - 1 <= horizontalLocation && horizontalLocation <= xRegister + 1);
-}
-
-void Day10::ProduceScreenOutput(const int cycle, const int xRegister, std::string& output) {
-    if (cycle % m_screenWidth == 0)
-        output.append("\n");
-    IsPixelLit(xRegister, cycle) ? output.append("#") : output.append(" ");
 }
--- aoc_2022/src/day10/Day10.h
+++ aoc_2022/src/day10/Day10.h
@@ -10,12 +10,15 @@ public:
 	std::vector<std::string> m_instructions;
 	const int m_screenWidth = 40;
 
+	typedef std::pair<int, int> state;
+
 private:
 	void Parse() override;
 	std::string Part1() override;
 	std::string Part2() override;
 
-	void UpdateInterestingValues(const int cycle, const int xRegister, std::map<int, int>& interestingSignalStrengths);
+	std::vector<state> ExecuteInstruction(int& cycle, int& xRegister, const std::string instruction);
+	void StatesToInterestingSignalStrengths(const std::vector<state> states, std::map<int, int>& interestingSignalStrengths);
+	void StatesToScreenOutput(const std::vector<state> states, std::string& output);
 	bool IsPixelLit(const int xRegister, const int cathodeRayIndex);
-	void ProduceScreenOutput(const int cycle, const int xRegister, std::string& output);
 };
