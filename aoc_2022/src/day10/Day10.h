f638f0c9f76bc2bae6595d42352393dc9ed3a804
Josh Nicholson <jsnicholson08@gmail.com>
2022-12-11 00:16:26 +0000

    remove unused function

--- aoc_2022/src/day10/Day10.h
+++ aoc_2022/src/day10/Day10.h
@@ -19,6 +19,5 @@ private:
 	std::map<int,int> ProcessAddXCommand(const int addValue, int& cycle, int& xRegister);
 	void UpdateInterestingValues(const std::map<int,int> signalStrengths, std::map<int,int>& interestingSignalStrengths);
 	bool IsPixelLit(const int xRegister, const int cathodeRayIndex);
-	void RunCycle(int& cycle);
 	void ProduceScreenOutput(const int cycle, const int xRegister, std::string& output);
 };
