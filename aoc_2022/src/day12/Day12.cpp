741695601997139af4aeb691e674a8679b46342c
Josh Nicholson <jonic@netcompany.com>
2022-12-20 17:16:16 +0000

    create graph for map

--- aoc_2022/src/day12/Day12.cpp
+++ aoc_2022/src/day12/Day12.cpp
@@ -1,6 +1,23 @@
 #include "Day12.h"
 
+#include <algorithm>
+
+// directions l,u,r,d
+const std::vector<std::pair<int, int>> directions{ {-1,0},{0,1},{1,0},{0,-1} };
+
 void Day12::Parse() {
+    m_gridSize = std::make_pair(data[0].size(), data.size());
+    size_t x, y;
+    for (y = 0; y < m_gridSize.second; y++) {
+        for (x = 0; x < m_gridSize.first; x++) {
+            coordinate coord = std::make_pair(x, y);
+            CheckForStartAndEnd(coord);
+            nodename name = CoordToName(coord);
+            edges edges = GetNeighbours(coord);
+
+            m_graph.insert(std::make_pair(name,edges));
+        }
+    }
 }
 
 std::string Day12::Part1() {
@@ -9,4 +26,50 @@ std::string Day12::Part1() {
 
 std::string Day12::Part2() {
     return std::string("unimplemented");
+}
+
+Day12::nodename Day12::CoordToName(coordinate coord) {
+    std::string name;
+    name.append(std::to_string(coord.first)); name += ','; name.append(std::to_string(coord.second)); name += ':'; name += GetValue(coord);
+    return name;
+}
+
+void Day12::CheckForStartAndEnd(Day12::coordinate coord) {
+    auto value = data[coord.second][coord.first];
+    if (value == 'S')
+        m_start = coord;
+    else if (value == 'E')
+        m_end = coord;
+}
+
+char Day12::GetValue(Day12::coordinate coord) {
+    char value = data[coord.second][coord.first];
+    if (value == 'S')
+        value = 'a';
+    else if (value == 'E')
+        value = 'z';
+    return value;
+}
+
+std::vector<Day12::nodename> Day12::GetNeighbours(coordinate coord) {
+    std::vector<nodename> neighbours{};
+    auto value = GetValue(coord);
+    for (const auto& direction : directions) {
+        coordinate neighbourCoord = std::make_pair(coord.first + direction.first, coord.second + direction.second);
+        if (!IsCoordinateInGrid(neighbourCoord, m_gridSize))
+            continue;
+        auto neighbourValue = data[neighbourCoord.second][neighbourCoord.first];
+        if (IsValidNeighbour(value, neighbourValue))
+            neighbours.push_back(CoordToName(neighbourCoord));
+    }
+    return neighbours;
+}
+
+bool Day12::IsCoordinateInGrid(Day12::coordinate coord, Day12::coordinate gridSize) {
+    return (coord.first >= 0 && coord.first < gridSize.first) &&
+        (coord.second >= 0 && coord.second < gridSize.second);
+}
+
+bool Day12::IsValidNeighbour(int value, int neighbourValue) {
+    return neighbourValue < value + 1;
 }
--- aoc_2022/src/day12/Day12.h
+++ aoc_2022/src/day12/Day12.h
@@ -2,11 +2,27 @@
 #include "Day.h"
 
 #include <string>
+#include <unordered_map>
 
 class Day12 : public Day {
+public:
+	typedef std::pair<size_t, size_t> coordinate;
+	typedef std::string nodename;
+	typedef std::vector<nodename> edges;
+	std::unordered_map<nodename, edges> m_graph;
+
+	coordinate m_gridSize, m_start, m_end;
 
 private:
 	void Parse() override;
 	std::string Part1() override;
 	std::string Part2() override;
+
+public:
+	std::string CoordToName(coordinate);
+	void CheckForStartAndEnd(coordinate coord);
+	std::vector<nodename> GetNeighbours(coordinate);
+	bool IsCoordinateInGrid(coordinate coord, coordinate gridSize);
+	bool IsValidNeighbour(int value, int neighbourValue);
+	char GetValue(coordinate coord);
 };
