d94e3ee53411780de86fe56402d9870239512ca9
Joshua Nicholson <jsnicholson08@gmail.com>
2023-12-07 15:54:18 +0000

    completed day4 part1

--- aoc_2023/src/day4/Day4.cpp
+++ aoc_2023/src/day4/Day4.cpp
@@ -2,13 +2,53 @@
 
 #include "Utils.hpp"
 
+#include <algorithm>
+
 void Day4::Parse() {
+    for (std::vector<std::string>::iterator it = data.begin(); it != data.end(); it++) {
+        card card;
+        std::string line = *it;
+        std::vector<std::string> vecStringCards = Utils::Split(line, ": ");
+        card.id = std::stoi(Utils::Split(vecStringCards[0], ' ').back());
+        std::vector<std::string> vecStringNumbers = Utils::Split(vecStringCards[1], " | ");
+        std::vector<std::string> vecStringWinningNumbers = Utils::Split(vecStringNumbers[0], ' ');
+        std::erase_if(vecStringWinningNumbers, [](const auto& s) {
+            return s.length() == 0;
+            });
+        std::vector<std::string> vecStringPlayedNumbers = Utils::Split(vecStringNumbers[1], ' ');
+        std::erase_if(vecStringPlayedNumbers, [](const auto& s) {
+            return s.length() == 0;
+            });
+
+        for (const auto& strNumber : vecStringWinningNumbers) {
+            card.winningNumbers.push_back(std::stoi(strNumber));
+        }
+        for (const auto& strNumber : vecStringPlayedNumbers) {
+            card.playedNumbers.push_back(std::stoi(strNumber));
+        }
+        m_cards.push_back(card); 
+    }
 }
 
 std::string Day4::Part1() {
-    return std::to_string(0);
+    int totalPoints = 0;
+    for (const card card : m_cards) {
+        totalPoints += CalculateCardPoints(card);
+    }
+    return std::to_string(totalPoints);
 }
 
 std::string Day4::Part2() {
     return std::to_string(0);
+}
+
+int Day4::CalculateCardPoints(card card) {
+    int cardPoints = 0;
+    for (const int playedNumber : card.playedNumbers) {
+        if (std::find(card.winningNumbers.begin(), card.winningNumbers.end(), playedNumber) == card.winningNumbers.end())
+            continue;
+
+        (cardPoints == 0) ? cardPoints = 1 : cardPoints = cardPoints * 2;
+    }
+    return cardPoints;
 }
--- aoc_2023/src/day4/Day4.h
+++ aoc_2023/src/day4/Day4.h
@@ -2,8 +2,21 @@
 #include "Day.h"
 
 class Day4 : public Day {
+public:
+	struct card {
+		int id;
+		std::vector<int> winningNumbers;
+		std::vector<int> playedNumbers;
+	};
+
 private:
 	void Parse() override;
 	std::string Part1() override;
 	std::string Part2() override;
+
+public:
+	int CalculateCardPoints(card card);
+
+public:
+	std::vector<card> m_cards;
 };
