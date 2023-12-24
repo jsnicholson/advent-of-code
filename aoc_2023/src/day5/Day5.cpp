c1046e169479d692ca664012ba349006753ea80a
Joshua Nicholson <jsnicholson08@gmail.com>
2023-12-24 22:05:40 +0000

    completed day5 part1

--- aoc_2023/src/day5/Day5.cpp
+++ aoc_2023/src/day5/Day5.cpp
@@ -1,12 +1,89 @@
 #include "Day5.h"
 
+#include "Utils.hpp"
+
 void Day5::Parse() {
+    auto seedStrings = Utils::Split(data.front(), ' ');
+    seedStrings.erase(seedStrings.begin());
+    for (auto seedString : seedStrings) {
+        m_seedNumbers.push_back(std::stoul(seedString));
+    }
+
+    Day5::mapper mapper;
+    for (std::vector<std::string>::iterator it = data.begin()+=3; it != data.end(); it++) {
+        std::string line = *it;
+        if (line.empty()) {
+            m_maps.push_back(mapper);
+            mapper.clear();
+            it++;
+            continue;
+        }
+
+        if (std::isdigit(line[0])) {
+            auto ruleString = Utils::Split(line, ' ');
+            auto destinationMin = std::stoul(ruleString[0]);
+            auto sourceMin = std::stoul(ruleString[1]);
+            auto range = std::stoul(ruleString[2]);
+            rule rule;
+            rule.sourceMin = sourceMin;
+            rule.sourceMax = sourceMin + range;
+            rule.destinationMin = destinationMin;
+            mapper.push_back(rule);
+            continue;
+        }
+
+        // is name line
+        continue;
+    }
+    // push mapper that reached end of file
+    m_maps.push_back(mapper);
 }
 
 std::string Day5::Part1() {
-    return std::to_string(0);
+    unsigned long currentMin;
+    for (auto seed : m_seedNumbers) {
+        auto value = seed;
+        for (std::vector<mapper>::iterator it = m_maps.begin(); it != m_maps.end(); it++) {
+            value = MapValue(*it, value);
+        }
+        currentMin = std::min(currentMin, value);
+    }
+    return std::to_string(currentMin);
 }
 
 std::string Day5::Part2() {
-    return std::to_string(0);
+    std::vector<std::pair<unsigned long, unsigned long>> ranges;
+    unsigned long long totalToSearch = {0};
+    for (std::vector<unsigned long>::iterator it = m_seedNumbers.begin(); it != m_seedNumbers.end(); it+=2) {
+        unsigned long rangeStart = *it;
+        unsigned long rangeLength = *(it + 1);
+        ranges.push_back(std::make_pair(rangeStart,rangeStart+rangeLength));
+        totalToSearch += rangeLength;
+    }
+
+    unsigned long currentMin;
+    unsigned long long currentSearch = { 0 };
+    for (auto range : ranges) {
+        for (unsigned long i = range.first; i <= range.second; i++) {
+            currentSearch += 1;
+            std::cout << "currently searching " << currentSearch << " of " << totalToSearch << std::endl;
+            auto value = i;
+            for (std::vector<mapper>::iterator it = m_maps.begin(); it != m_maps.end(); it++) {
+                value = MapValue(*it, value);
+            }
+            currentMin = std::min(currentMin, value);
+        }
+    }
+    return std::to_string(currentMin);
+}
+
+unsigned long Day5::MapValue(mapper mapper, unsigned long value) {
+    for (auto rule : mapper) {
+        if (value >= rule.sourceMin
+            && value <= rule.sourceMax) {
+            unsigned long offset = value - rule.sourceMin;
+            return rule.destinationMin + offset;
+        }
+    }
+    return value;
 }
--- aoc_2023/src/day5/Day5.h
+++ aoc_2023/src/day5/Day5.h
@@ -2,8 +2,24 @@
 #include "Day.h"
 
 class Day5 : public Day {
+public:
+	struct rule {
+		unsigned long sourceMin, sourceMax;
+		unsigned long destinationMin;
+	};
+
+	typedef std::vector<rule> mapper;
+	typedef std::map<unsigned long, unsigned long> improvedMapper;
+
 private:
 	void Parse() override;
 	std::string Part1() override;
 	std::string Part2() override;
+
+public:
+	unsigned long MapValue(mapper mapper, unsigned long value);
+
+public:
+	std::vector<unsigned long> m_seedNumbers;
+	std::vector<mapper> m_maps;
 };
