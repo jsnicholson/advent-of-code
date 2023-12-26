6e5c2f987a3ce5d42458b83700cc3caaa750a73a
Joshua Nicholson <jsnicholson08@gmail.com>
2023-12-26 12:07:54 +0000

    completed day6 part1

--- aoc_2023/src/day6/Day6.cpp
+++ aoc_2023/src/day6/Day6.cpp
@@ -1,15 +1,53 @@
 #include "Day6.h"
 
-#include <set>
-#include <cmath>
+#include "Utils.hpp"
+#include <format>
 
 void Day6::Parse() {
+    std::string times = data[0];
+    auto timeTokens = Utils::Split(times, ' ');
+    std::erase_if(timeTokens, [](const auto& s) {
+        return s.length() == 0;
+        });
+    timeTokens.erase(timeTokens.begin());
+
+    std::string distances = data[1];
+    auto distanceTokens = Utils::Split(distances, ' ');
+    std::erase_if(distanceTokens, [](const auto& s) {
+        return s.length() == 0;
+        });
+    distanceTokens.erase(distanceTokens.begin());
+
+    for (int i = 0; i < timeTokens.size(); i++) {
+        race r = {
+            std::stoi(distanceTokens[i]),
+            std::stoi(timeTokens[i])
+        };
+        m_races.push_back(r);
+    }
 }
 
 std::string Day6::Part1() {
-    return std::to_string(0);
+    int total = 1;
+    for (const auto& race : m_races) {
+        auto intersections = SolveForTime(race.length, race.distance);
+        int waysToBeat = std::ceil(intersections.second) - std::floor(intersections.first) - 1;
+        total *= waysToBeat;
+    }
+    return std::to_string(total);
 }
 
 std::string Day6::Part2() {
     return std::to_string(0);
+}
+
+int Day6::SolveForDistance(int raceLength, int timeButtonHeld) {
+    return (raceLength - timeButtonHeld) * timeButtonHeld;
+}
+
+std::pair<double, double> Day6::SolveForTime(int raceLength, int raceDistance) {
+    std::pair<double, double> timesButtonHeld;
+    timesButtonHeld.first = (raceLength - std::sqrt(raceLength * raceLength - 4 * raceDistance)) / 2;
+    timesButtonHeld.second = (raceLength + std::sqrt(raceLength * raceLength - 4 * raceDistance)) / 2;
+    return timesButtonHeld;
 }
--- aoc_2023/src/day6/Day6.h
+++ aoc_2023/src/day6/Day6.h
@@ -2,8 +2,21 @@
 #include "Day.h"
 
 class Day6 : public Day {
+public:
+	struct race {
+		int distance;
+		int length;
+	};
+
 private:
 	void Parse() override;
 	std::string Part1() override;
 	std::string Part2() override;
+
+public:
+	int SolveForDistance(int raceLength, int timeButtonHeld);
+	std::pair<double, double> SolveForTime(int raceLength, int raceDistance);
+
+public:
+	std::vector<race> m_races;
 };
