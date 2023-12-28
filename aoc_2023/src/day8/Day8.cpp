b2d14bef6e102f5a77833850bad2d760bcab52b8
Joshua Nicholson <jsnicholson08@gmail.com>
2023-12-28 19:49:40 +0000

    completed day8 part1

--- aoc_2023/src/day8/Day8.cpp
+++ aoc_2023/src/day8/Day8.cpp
@@ -1,10 +1,31 @@
 #include "Day8.h"
 
+#include "Utils.hpp"
+
 void Day8::Parse() {
+    m_instructions = *data.begin();
+    for (std::vector<std::string>::const_iterator it = data.begin() + 2; it != data.end(); it++) {
+        auto tokens = Utils::Split(*it, " = ");
+        auto instructionTokens = Utils::Split(tokens[1], ", ");
+        m_nodes.insert(std::make_pair(tokens[0], node {
+                tokens[0],
+                instructionTokens[0].substr(1),
+                instructionTokens[1].substr(0,3)
+            }));
+    }
 }
 
 std::string Day8::Part1() {
-    return(std::to_string(0));
+    int stepsTaken = 0;
+    node currentNode = m_nodes.at("AAA");
+    while (currentNode.name != "ZZZ") {
+        int index = stepsTaken % m_instructions.length();
+        char instruction = m_instructions[index];
+        std::string nextNodeName = (instruction == 'L') ? m_nodes.at(currentNode.left).name : m_nodes.at(currentNode.right).name;
+        currentNode = m_nodes.at(nextNodeName);
+        stepsTaken++;
+    }
+    return(std::to_string(stepsTaken));
 }
 
 std::string Day8::Part2() {
--- aoc_2023/src/day8/Day8.h
+++ aoc_2023/src/day8/Day8.h
@@ -1,9 +1,22 @@
 #pragma once
 #include "Day.h"
 
+#include <map>
+
 class Day8 : public Day {
+public:
+	struct node {
+		std::string name;
+		std::string left;
+		std::string right;
+	};
+
 private:
 	void Parse() override;
 	std::string Part1() override;
 	std::string Part2() override;
+
+public:
+	std::string m_instructions;
+	std::map<std::string, node> m_nodes;
 };
