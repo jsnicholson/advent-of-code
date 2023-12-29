b553939bcfcfd5b2dbefb0e4993108b94d57b5ee
Joshua Nicholson <jsnicholson08@gmail.com>
2023-12-29 20:13:31 +0000

    clean up for part2

--- aoc_2023/src/day10/Day10.cpp
+++ aoc_2023/src/day10/Day10.cpp
@@ -16,21 +16,7 @@ void Day10::Parse() {
 }
 
 std::string Day10::Part1() {
-    coord currentCoord = m_startCoord;
-    char currentPipe;
-    coord lastDirection = {};
-    std::vector<coord> vecCoordsPartOfLoop = {};
-    do {
-        currentPipe = m_grid[currentCoord.second][currentCoord.first];
-        std::vector<coord> validDirections = mapPipeToDirections.at(currentPipe);
-        std::vector<coord>::iterator it = std::find(validDirections.begin(), validDirections.end(), lastDirection);
-        if(it != validDirections.end())
-            validDirections.erase(it);
-        const std::pair<char, coord> foundConnectingNeighbour = FindConnectingNeighbour(currentCoord, validDirections);
-        vecCoordsPartOfLoop.push_back(foundConnectingNeighbour.second);
-        lastDirection = Utils::subtract(currentCoord, foundConnectingNeighbour.second);
-        currentCoord = foundConnectingNeighbour.second;
-    } while (currentCoord != m_startCoord);
+    std::vector<coord> vecCoordsPartOfLoop = CalculateLoop(m_startCoord);
     return std::to_string(((int)vecCoordsPartOfLoop.size()/2));
 }
 
@@ -38,7 +24,30 @@ std::string Day10::Part2() {
     return std::to_string(0);
 }
 
-std::pair<char, Day10::coord> Day10::FindConnectingNeighbour(const coord& startingCoord, const std::vector<coord>& directions) {
+std::vector<Day10::coord> Day10::CalculateLoop(const coord& startCoord) {
+    coord currentCoord = startCoord;
+    direction lastDirection = {};
+    std::vector<coord> vecCoordsPartOfLoop = {};
+    do {
+        std::vector<direction> validDirections = GetValidDirections(currentCoord, lastDirection);
+        const std::pair<char, coord> foundConnectingNeighbour = FindConnectingNeighbour(currentCoord, validDirections);
+        vecCoordsPartOfLoop.push_back(foundConnectingNeighbour.second);
+        lastDirection = Utils::subtract(currentCoord, foundConnectingNeighbour.second);
+        currentCoord = foundConnectingNeighbour.second;
+    } while (currentCoord != m_startCoord);
+    return vecCoordsPartOfLoop;
+}
+
+std::vector<Day10::direction> Day10::GetValidDirections(const coord& fromCoord, const direction& lastDirection) {
+    char currentPipe = m_grid[fromCoord.second][fromCoord.first];
+    std::vector<direction> validDirections = mapPipeToDirections.at(currentPipe);
+    std::vector<direction>::iterator it = std::find(validDirections.begin(), validDirections.end(), lastDirection);
+    if (it != validDirections.end())
+        validDirections.erase(it);
+    return validDirections;
+}
+
+std::pair<char, Day10::coord> Day10::FindConnectingNeighbour(const coord& startingCoord, const std::vector<direction>& directions) {
     for (const auto& direction : directions) {
         coord coordToCheck = Utils::add(startingCoord, direction);
         char foundPipe = m_grid[coordToCheck.second][coordToCheck.first];
@@ -50,4 +59,22 @@ std::pair<char, Day10::coord> Day10::FindConnectingNeighbour(const coord& starti
     }
 
     return std::make_pair(NULL,none);
+}
+
+int Day10::ShoeLace(const std::vector<coord>& path) {
+    if (path.size() <= 1) {
+        return 0;
+    }
+
+    int result = 0;
+    for (size_t i = 0; i < path.size() - 1; ++i) {
+        int x1 = path[i].first;
+        int y1 = path[i].second;
+        int x2 = path[i + 1].first;
+        int y2 = path[i + 1].second;
+
+        result += (y1 + y2) * (x2 - x1);
+    }
+
+    return result;
 }
--- aoc_2023/src/day10/Day10.h
+++ aoc_2023/src/day10/Day10.h
@@ -6,14 +6,15 @@
 class Day10 : public Day {
 public:
 	typedef std::pair<int, int> coord;
+	typedef std::pair<int, int> direction;
 
-	static inline const coord up = { 0, -1 };
-	static inline const coord right = { 1, 0 };
-	static inline const coord down = { 0, 1 };
-	static inline const coord left = { -1, 0 };
-	static inline const coord none = { 0, 0 };
+	static inline const direction up = { 0, -1 };
+	static inline const direction right = { 1, 0 };
+	static inline const direction down = { 0, 1 };
+	static inline const direction left = { -1, 0 };
+	static inline const direction none = { 0, 0 };
 
-	static inline const std::map<coord, int> mapVectorToIndex = {
+	static inline const std::map<direction, int> mapVectorToIndex = {
 		{up,0},{right,1},{down,2},{left,3}
 	};
 	
@@ -26,7 +27,7 @@ public:
 	* 
 	* can convert using map mapVectorToIndex
 	*/
-	static inline const std::map<char, std::vector<coord>> mapPipeToDirections = {
+	static inline const std::map<char, std::vector<direction>> mapPipeToDirections = {
 		{'|', {up, down}},
 		{'-', {right, left}},
 		{'L', {up, right}},
@@ -39,7 +40,7 @@ public:
 	/*
 	* if we look in direction, what pipes are valid?
 	*/
-	static inline const std::map<coord,std::vector<char>> mapDirectionToPipes = {
+	static inline const std::map<direction,std::vector<char>> mapDirectionToPipes = {
 		{up, {'|','7','F','S'}},
 		{right, {'-','J','7','S'}},
 		{down, {'|','L','J','S'}},
@@ -52,7 +53,10 @@ private:
 	std::string Part2() override;
 
 public:
-	std::pair<char, coord> FindConnectingNeighbour(const coord&,const std::vector<coord>&);
+	std::vector<coord> CalculateLoop(const coord& startCoord);
+	std::vector<direction> GetValidDirections(const coord& fromCoord, const direction& lastDirection);
+	std::pair<char, coord> FindConnectingNeighbour(const coord&,const std::vector<direction>&);
+	int ShoeLace(const std::vector<coord>& path);
 
 public:
 	coord m_startCoord;
