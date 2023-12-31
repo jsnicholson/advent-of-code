d2df021e18f770c1711b702a10fb2170c9f5dfab
Joshua Nicholson <jsnicholson08@gmail.com>
2023-12-31 13:16:11 +0000

    started day11 part1 got the expanded universe part working

--- aoc_2023/src/day11/Day11.h
+++ aoc_2023/src/day11/Day11.h
@@ -1,71 +1,24 @@
 #pragma once
 #include "Day.h"
 
-#include <functional>
-#include <map>
-
-typedef uint64_t item;
-
-class Monkey {
-public:
-	// plainTextDescription needs to be parsed to fill out members
-	Monkey(std::string plainTextDescription);
-
-private:
-	std::vector<item> m_items;
-	std::function<void(item&)> m_operation;
-	int m_testDivisibleBy;
-	size_t m_ifTestTrueThrowToMonkeyNumber, m_ifTestFalseThrowToMonkeyNumber;
-	int m_itemsInspected;
-
-private:
-	const std::string m_tokenStartingItems = "Starting items: ";
-	const std::string m_tokenOperation = "Operation: ";
-	const std::string m_tokenTestDivisbleBy = "Test: ";
-	const std::string m_tokenTrue = "If true: ";
-	const std::string m_tokenFalse = "If false: ";
-
-	const enum TermType {
-		constant = 1,
-		variable = 2
-	};
-
-	const std::map<std::string, std::function<void(std::string)>> m_mapTokensToProcess = {
-		{ m_tokenStartingItems, [=](std::string string) {this->ProcessStartingItems(string); } },
-		{ m_tokenOperation, [=](std::string string) {this->ProcessOperation(string); } },
-		{ m_tokenTestDivisbleBy, [=](std::string string) {this->ProcessTest(string); } },
-		{ m_tokenTrue, [=](std::string string) {this->ProcessTestTrue(string); } },
-		{ m_tokenFalse, [=](std::string string) {this->ProcessTestFalse(string); } }
-	};
-
-private:
-	void ProcessStartingItems(const std::string);
-	void ProcessOperation(const std::string);
-	void ProcessTest(const std::string);
-	void ProcessTestTrue(const std::string);
-	void ProcessTestFalse(const std::string);
-
-	void InspectFirstItem();
-	size_t DecideWhereToThrowItem();
-
-public:
-	void InspectAllItems();
-	
-	void ThrowItem(const size_t monkeyToThrowTo);
-	void CatchItem(const item itemCaught);
-	int GetItemsInspectedCount() { return m_itemsInspected; }
-	void ShowItems();
-};
-
 class Day11 : public Day {
 public:
-	std::vector<Monkey*> m_monkeys;
+	typedef std::vector<std::string> universe;
+	typedef std::pair<int, int> coord;
 
 private:
 	void Parse() override;
 	std::string Part1() override;
 	std::string Part2() override;
 
-	bool DoesStringStartWithToken(const std::string string, const std::string token);
-	void CatchItems();
+public:
+	int CalculateShortestDiscreteDistanceBetweenPoints(const coord& a, const coord& b);
+	universe CalculateExpandedUniverse(const universe& originalUniverse);
+	void AddRowsToUniverse(universe& universe, std::vector<size_t> rows);
+	void AddColumnsToUniverse(universe& universe, std::vector<size_t> columns);
+	std::vector<size_t> GetIndicesOfEmptyRows(const universe& universe);
+	std::vector<size_t> GetIndicesOfEmptyColumns(const universe& universe);
+
+public:
+	universe m_universe;
 };
